ffb88268e75507c61b942694f3d1aaff34975c0e
Mafaz Zafar <mzafar@ahslink.com>
2016-11-19 08:27:00 -0600

    Add postgres for production

--- db.js
+++ db.js
@@ -1,9 +1,19 @@
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
 
-var sequelize = new Sequelize(undefined, undefined, undefined, {
+if (env ==='production') {
+	sequelize = new Sequelize(process.env.DATABSE_URL, {
+	'dialect': 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(undefined, undefined, undefined, {
 	'dialect': 'sqlite',
 	'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+	});
+}
+
+
 
 //We will attach multiple properties with db object and eventual export it from db.js
 var db = {};
